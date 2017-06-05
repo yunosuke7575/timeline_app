17d24636252d652cd8a6d6bf047a703214a63ad7
Yunosuke Katayama <purio.k1231@gmail.com>
2017-06-06 07:03:52 +0900

    twitter apiを使用し、タイムラインを取得

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,14 @@
 class HomeController < ApplicationController
+
+	require 'twitter'
+
+
   def top
+  	@client = Twitter::REST::Client.new do |config|
+      config.consumer_key        = ENV['TW_CONSUMER_KEY']
+      config.consumer_secret     = ENV['TW_CONSUMER_SECRET']
+      config.access_token        = ENV['TW_ACCESS_TOKEN']
+      config.access_token_secret = ENV['TW_ACCESS_TOKEN_SECRET']
   end
+end
 end
