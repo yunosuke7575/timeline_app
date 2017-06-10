811622d8f2a9de1c37c57a4c1cf94fce7a1815d8
Yunosuke Katayama <purio.k1231@gmail.com>
2017-06-11 03:51:52 +0900

    タイムラインを時系列表示に変更

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
 
 	require 'twitter'
-
+  require 'time'
 
   def top
   	@client = Twitter::REST::Client.new do |config|
@@ -9,9 +9,19 @@ class HomeController < ApplicationController
       config.consumer_secret     = ENV['TW_CONSUMER_SECRET']
       config.access_token        = ENV['TW_ACCESS_TOKEN']
       config.access_token_secret = ENV['TW_ACCESS_TOKEN_SECRET']
+    end
+
+    tweets = @client.user_timeline.first(20)
+
+    popular = Instagram.user_recent_media
+    popular.each do |insta|
+      insta[:created_at] = Time.at(insta[:created_time].to_i)
+    end
+
+    @posts = (tweets + popular).sort_by {|post| post.created_at}.reverse!
   end
-  @client = @client.user_timeline.first(20)
-  @popular = Instagram.user_recent_media
-  @posts = @client + @popular
 end
-end
+
+ 
+    
+
